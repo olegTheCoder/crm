cce80e6e0c2be04f536d124484aad703ce34135a
Oleg Grachev <91218806+olegTheCoder@users.noreply.github.com>
2021-12-10 16:22:35 +0200

    Update database.js

--- db/database.js
+++ db/database.js
@@ -8,9 +8,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-      ssl: {rejectUnauthorized: false},
-    },
+    
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -18,9 +16,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-      ssl: {rejectUnauthorized: false},
-    },
+    
   },
   production: {
     username: process.env.DB_USERNAME,
